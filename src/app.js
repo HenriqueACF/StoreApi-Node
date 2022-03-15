580e13bef0d3914cf9d628ad9eed3d4ca862cb1c
henriqueacf <henrique.assis145@gmail.com>
2022-03-15 15:53:01 -0300

    refactor(routes): ajusting routes

--- src/app.js
+++ src/app.js
@@ -1,14 +1,26 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 const router = express.Router()
 
-const route = router.get('/', (req, res, next)=>{
-    res.status(200).send({
-        title:'Node API',
-        version:'0.0.1'
-    })
-})
-app.use('/', route)
+//IMPORTANDO AS ROTAS
+const indexRoutes = require('./routes/indexRoutes')
+const productRoute = require('./routes/productRoute')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use('/', indexRoutes)
+app.use('/products', productRoute)
 
 module.exports = app;
+
+
+//STATUS HTTP
+//200 -> OK
+// 201 -> CRIADO
+// 400 -> BADREQUEST
+//401 -> NAO AUTENTICADO
+//403 -> NAO AUTORIZADO
+//500 -> ERRO INTERNO
