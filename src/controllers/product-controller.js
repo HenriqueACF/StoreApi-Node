eda48dd9c467bf9c2122c6691dca577eab5adb6c
henriqueacf <henrique.assis145@gmail.com>
2022-03-16 13:47:01 -0300

    feat(product-controller): add a validation(contract) in create product

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 const Product = mongoose.model('Product')
+const ValidationContract = require('../validators/fluent-validator');
+
 //GET PRODUCTS
 exports.get =(req, res, next) =>{
     Product
@@ -51,6 +53,17 @@ exports.getByTag =(req, res, next) =>{
 }
 
 exports.post = (req,res,next) =>{
+    let contract = new ValidationContract()
+    contract.hasMinLen(req.body.title, 3, 'O titulo deve conter pelo menos 3 caracteres')
+    contract.hasMinLen(req.body.slug, 3, 'O slug deve conter pelo menos 3 caracteres')
+    contract.hasMinLen(req.body.description, 3, 'A descrição deve conter pelo menos 3 caracteres')
+
+    //VERIFICA SE OS DADOS SAO INVALIDOS
+    if(!contract.isValid()){
+        res.status(400).send(contract.errors()).end()
+        return
+    }
+
     var product = new Product(req.body)
     product
         .save()
