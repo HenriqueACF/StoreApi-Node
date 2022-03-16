e3a89a18ca13201c6a9d2f5e12a438b4eeaab128
henriqueacf <henrique.assis145@gmail.com>
2022-03-16 09:54:49 -0300

    feat:(product) add get products and get by tags,id, slug

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,5 +1,54 @@
 const mongoose = require('mongoose')
 const Product = mongoose.model('Product')
+//GET PRODUCTS
+exports.get =(req, res, next) =>{
+    Product
+        .find({active:true}, 'title price slug')
+        .then(data =>{
+            res.status(200).send(data)
+        }).catch(e =>{
+            res.status(400).send(e)
+    })
+}
+
+// GET PRODUCTS BY SLUG
+exports.getBySlug =(req, res, next) =>{
+    Product
+        .findOne({
+            slug:req.params.slug,
+            active:true
+        }, 'title description price slug tags')
+        .then(data =>{
+            res.status(200).send(data)
+        }).catch(e =>{
+        res.status(400).send(e)
+    })
+}
+
+// GET PRODUCTS BY ID
+exports.getById =(req, res, next) =>{
+    Product
+        .findById(req.params.id)
+        .then(data =>{
+            res.status(200).send(data)
+        }).catch(e =>{
+        res.status(400).send(e)
+    })
+}
+
+// GET PRODUCTS BY TAG
+exports.getByTag =(req, res, next) =>{
+    Product
+        .find({
+            tags: req.params.tag,
+            active: true
+        })
+        .then(data =>{
+            res.status(200).send(data)
+        }).catch(e =>{
+        res.status(400).send(e)
+    })
+}
 
 exports.post = (req,res,next) =>{
     var product = new Product(req.body)
