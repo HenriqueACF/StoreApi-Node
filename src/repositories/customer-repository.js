e9f874e7af64df385f2218b8bbd627a911d3b267
henriqueacf <henrique.assis145@gmail.com>
2022-03-21 09:33:50 -0300

    feat:(auth-service): foi adcionado o JWT

--- src/repositories/customer-repository.js
+++ src/repositories/customer-repository.js
@@ -5,3 +5,12 @@ exports.create = async(data) => {
     var customer = new Customer(data);
     await customer.save();
 }
+
+exports.authenticate = async(data) => {
+    const res = await Customer.findOne({
+        email: data.email,
+        password: data.password
+    });
+    return res;
+}
+
